6853605cb6085c3a00769b3e8b3b05019d68223b
Anirban <anirbancodes@gmail.com>
2025-05-06 03:17:46 +0530

    create events & attendees rest api with api resources for events and attendees

---
+++ database/seeders/AttendeeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Attendee;
+use App\Models\User;
+use App\Models\Event;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AttendeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = User::all();
+        $events = Event::all();
+
+        foreach ($users as $user) {
+            $eventsToAttend = $events->random(rand(1, 3));
+            foreach ($eventsToAttend as $eventToAttend) {
+                Attendee::create(['user_id' => $user->id, 'event_id' => $eventToAttend->id]);
+            }
+        }
+    }
+}
